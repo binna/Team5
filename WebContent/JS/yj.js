9d169f4e6d2d8f25268f9f36b93ead2cc2e39beb
Yoonjoo0528 <yjnam2865@gmail.com>
2020-06-03 17:26:17 +0900

    0603 yj :package 4개 (yoon), jsp파일,css, js

--- WebContent/JS/yj.js
+++ WebContent/JS/yj.js
@@ -1,1 +1,15 @@
 // 윤주
+
+$(document).ready(function(){
+	
+	$("#product_main_nav").slideUp(0);
+	
+	$("#category_li").mouseenter(function(){
+		$("#product_main_nav").slideDown(300);
+
+	});
+	$("#product_main_nav").mouseleave(function(){
+		$("#product_main_nav").slideUp(300);
+
+	});
+});
